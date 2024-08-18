c03dc38afd518a5b2eb01048674e9d34c5447be0
Andrew Johnson <andrewmjohnson549@gmail.com>
2024-08-18 10:26:46 +0200

    Rename and add install script

---
+++ bin/install_ros_deps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# Install ROS dependencies
+DEBIAN_FRONTEND=noninteractive \
+    apt-get update &&
+    apt-get install -y \
+        ros-${ROS_DISTRO}-xacro \
+        ros-${ROS_DISTRO}-rosbridge-server \
+        ros-${ROS_DISTRO}-rosbridge-suite \
+        ros-${ROS_DISTRO}-tf \
+        ros-${ROS_DISTRO}-cv-bridge \
+        ros-${ROS_DISTRO}-image-transport \
+        ros-${ROS_DISTRO}-laser-geometry \
+        ros-${ROS_DISTRO}-roslint \
+        ros-${ROS_DISTRO}-foxglove-bridge \
+        ros-${ROS_DISTRO}-rqt-tf-tree \
+        ros-${ROS_DISTRO}-diagnostic-updater \
+        ros-${ROS_DISTRO}-rqt \
+        ros-${ROS_DISTRO}-rqt-common-plugins \
+        ros-${ROS_DISTRO}-robot-state-publisher \
+        ros-${ROS_DISTRO}-gazebo-ros-pkgs \
+        ros-${ROS_DISTRO}-gazebo-ros-control \
+        ros-${ROS_DISTRO}-controller-manager \
+        ros-${ROS_DISTRO}-controller-interface \
+        ros-${ROS_DISTRO}-joint-state-controller \
+        ros-${ROS_DISTRO}-gazebo-ros-control \
+        ros-${ROS_DISTRO}-ros-controllers \
+        ros-${ROS_DISTRO}-costmap-2d \
+        ros-${ROS_DISTRO}-navigation
